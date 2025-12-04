dace59b01d75a86af0eab9a8766ea30de8163b3c
tastruken <tastruken@gmail.com>
2025-12-03 23:40:43 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    base: '/Boosted-Teoria-Org/',
     const env = loadEnv(mode, '.', '');
     return {
         server: {
@@ -24,4 +25,5 @@ export default defineConfig(({ mode }) => {
     }
     };
 });
+
 
